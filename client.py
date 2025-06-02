0d692568aedf7ecd978c9e068b61120b8357d66e
fatemeh-khajeh <72465357+fatemeh-khajeh@users.noreply.github.com>
2025-06-02 16:42:43 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 
-url = 'http://localhost:8000/test'
+# url = 'http://localhost:8000/test'
+url = 'https://rcw-test-g8efatdnbsc4d7a6.canadaeast-01.azurewebsites.net/test'
 
 response = requests.get(url)
 response = response.json()
