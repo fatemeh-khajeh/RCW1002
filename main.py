db3578f68cf60d2369291134ff6d638d510f9d27
fatemeh-khajeh <72465357+fatemeh-khajeh@users.noreply.github.com>
2025-06-02 14:36:01 -0400

    main.py updated

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI , HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    try:
+        return {"message": "Hello from RCW Teccart"}
+    except Exception as e:
+        print(f'Exception : {e}')
+        raise HTTPException(status_code = 500 , detail = str(e))
+    
+@app.get("/test")
+async def bienvenue():
+    try:
+        return {"message": "You are in RCW Groupe 1001"}
+    except Exception as e:
+        print(f'Exception : {e}')
+        raise HTTPException(status_code = 500 , detail = str(e))
